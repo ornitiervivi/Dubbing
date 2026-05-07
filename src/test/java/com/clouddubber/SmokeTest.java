b46a9810efd40b7fabac37cbe3408a9426afdaad
ornitiervivi <arionildo6@gmail.com>
2026-05-07 18:37:51 -0300

    fix: address review on smoke test bootstrap and compose datasource host

--- src/test/java/com/clouddubber/SmokeTest.java
+++ src/test/java/com/clouddubber/SmokeTest.java
@@ -1,7 +1,11 @@
 package com.clouddubber;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class SmokeTest {
     @Test
     void contextLoads() {
